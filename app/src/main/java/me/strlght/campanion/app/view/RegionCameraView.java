16eae7cc412555cdf64737a86f5f2639da5e4dd7
Grigoriy Dzhanelidze <grigoriy.dzanalidze@e-legion.com>
2014-10-21 13:58:56 +0400

    Started reworking RegionCameraView

--- app/src/main/java/me/strlght/campanion/app/view/RegionCameraView.java
+++ app/src/main/java/me/strlght/campanion/app/view/RegionCameraView.java
@@ -4,16 +4,18 @@ import android.content.Context;
 import android.graphics.*;
 import android.hardware.Camera;
 import android.util.AttributeSet;
-import android.widget.ImageView;
+import android.view.View;
 
 /**
  * Created by starlight on 10/12/14.
  */
-public class RegionCameraView extends ImageView {
+public class RegionCameraView extends View {
 
 	private Paint mPaint;
 	private Camera.Size mPreviewSize;
 	private Camera.Size mActualSize;
+	private Bitmap mRegion;
+	private float mRotation;
 
 	public RegionCameraView(Context context) {
 		super(context);
@@ -55,17 +57,34 @@ public class RegionCameraView extends ImageView {
 		}
 	}
 
+	@Override
+	protected void onDraw(Canvas canvas) {
+		if (mRegion != null) {
+			Matrix transform = new Matrix();
+			transform.postTranslate(-mRegion.getWidth() / 2, -mRegion.getHeight() / 2);
+			transform.postRotate(mRotation);
+			canvas.drawBitmap(mRegion, transform, null);
+		}
+	}
+
+	@Override
+	public void setRotation(float rotation) {
+		mRotation = rotation;
+		invalidate();
+	}
+
 	public void setSize(Camera.Size previewSize, Camera.Size actualSize) {
 		requestLayout();
+		// TODO: fix this
 		final int wph = getWidth() + getHeight();
 		final int length = Math.min(2048, wph);
 		mPreviewSize = previewSize;
 		mActualSize = actualSize;
 
 		if (length > 0 && wph != 0) {
-			Bitmap region = Bitmap.createBitmap(length, length, Bitmap.Config.ARGB_8888);
-			region.eraseColor(Color.parseColor("#00000000"));
-			Canvas canvas = new Canvas(region);
+			mRegion = Bitmap.createBitmap(length, length, Bitmap.Config.ARGB_8888);
+			mRegion.eraseColor(Color.parseColor("#00000000"));
+			Canvas canvas = new Canvas(mRegion);
 			canvas.drawColor(Color.parseColor("#CC000000"));
 			if (previewSize != null) {
 				float k = (float) mActualSize.width / mActualSize.height;
@@ -74,8 +93,6 @@ public class RegionCameraView extends ImageView {
 				int center = length / 2;
 				canvas.drawRect(center - scaledWidth / 2, center - scaledHeight / 2, center + scaledWidth / 2, center + scaledHeight / 2, mPaint);
 			}
-
-			setImageBitmap(region);
 		}
 	}
 
